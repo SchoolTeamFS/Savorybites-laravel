9881cad0e2c2a4043a72caedf4955839eddb9434
Imane Mrabet <mrabetimane78@gmail.com>
2025-02-10 20:46:15 +0100

    l

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 /*
